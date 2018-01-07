4124e74a1342fbe64fdeb6befa3cc46173b1791a
BurningNetel <joepseuren1@gmail.com>
2018-01-07 14:19:38 +0100

    reduce size of arrays to 200

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'crypto';
+const maxArraySize = 200;
 let db = undefined;
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, client) => {
@@ -15,22 +16,46 @@ MongoClient.connect(url, (err, client) => {
     db = client.db(dbName);
 });
 
+function getInterval(req) {
+    let timeInterval = 60 * 60 * 24; // 1 day in seconds
+    debug("query parameter 'interval': " + req.query.interval);
+    if (req.query.interval && parseInt(req.query.interval, 10)) {
+        timeInterval = req.query.interval;
+    }
+    debug("interval is " + timeInterval);
+    return timeInterval;
+}
+
+/**
+ * Returns an array that has every n'th element inside of the original array.
+ */
+function reduceSize(docs, maxSize) {
+    if(docs.size() < maxSize){
+        return docs;
+    }
+    let reducesDocs = [];
+    let n = Math.round(docs.size() / maxSize);
+
+    let i = 0;
+    for (let doc of docs) {
+        if (i % n === 0) {
+            reducesDocs.push(doc);
+        }
+        i++;
+    }
+    return reducesDocs;
+}
+
 /**
  * Get latest prices
  * By default 1 day of data
  */
 app.get('/api/prices', (req, res) => {
-    let timeInterval = 60 * 60 * 24; // 1 day in seconds
-    debug("query parameter 'interval': " + req.query.interval);
-    if(req.query.interval && parseInt(req.query.interval, 10)){
-        timeInterval = req.query.interval;
-    }
-    debug("interval is " + timeInterval);
-    // Get the documents collection
+    let timeInterval = getInterval(req);
+
     const collection = db.collection('prices');
-    // Find some documents
-    collection.find({date: { $gt: (new Date().getTime() / 1000) - timeInterval }}).toArray((err, docs) => {
-        res.send(docs)
+    collection.find({date: {$gt: (new Date().getTime() / 1000) - timeInterval}}).toArray((err, docs) => {
+        res.send(reduceSize(docs, maxArraySize))
     });
 });
 
@@ -39,17 +64,11 @@ app.get('/api/prices', (req, res) => {
  * By default 1 day of data
  */
 app.get('/api/balances', (req, res) => {
-    let timeInterval = 60 * 60 * 24; // 1 day in seconds
-    debug("query parameter 'interval': " + req.query.interval);
-    if(req.query.interval && parseInt(req.query.interval, 10)){
-        timeInterval = req.query.interval;
-    }
-    debug("interval is " + timeInterval);
-    // Get the documents collection
+    let timeInterval = getInterval(req);
+
     const collection = db.collection('balances');
-    // Find some documents
-    collection.find({date: { $gt: (new Date().getTime() / 1000) - timeInterval }}).toArray( (err, docs) => {
-        res.send(docs)
+    collection.find({date: {$gt: (new Date().getTime() / 1000) - timeInterval}}).toArray((err, docs) => {
+        res.send(reduceSize(docs, maxArraySize))
     });
 });
 
