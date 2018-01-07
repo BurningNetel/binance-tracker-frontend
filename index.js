1c6e1452c7bf14ae8779dca17e87460b81e4e482
BurningNetel <joepseuren1@gmail.com>
2018-01-07 11:30:34 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
 // Connection URL
@@ -10,39 +10,36 @@ const dbName = 'crypto';
 let db = undefined;
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, client) => {
-  console.log("Connected successfully to server");
-  db = client.db(dbName);
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
 });
-
 
 
 app.get('/api/prices', (req, res) => {
-  // Get the documents collection
-  const collection = db.collection('prices');
-  // Find some documents
-  collection.find().toArray(function(err, docs) {
-    console.log("Found the following records");
-    console.log(docs);
-    res.send(docs)
-  });
-})
+    // Get the documents collection
+    const collection = db.collection('prices');
+    // Find some documents
+    collection.find().toArray((err, docs) => {
+        res.send(docs)
+    });
+});
 
 
 app.get('/api/balances', (req, res) => {
-  // Get the documents collection
-  const collection = db.collection('balances');
-  // Find some documents
-  collection.find().toArray(function(err, docs) {
-    console.log(docs);
-    res.send(docs)
-  });
-})
+    // Get the documents collection
+    const collection = db.collection('balances');
+    // Find some documents
+    collection.find().toArray( (err, docs) => {
+        res.send(docs)
+    });
+});
 
-app.use('/js', express.static(path.join(__dirname, '/public/js')))
-app.use('/css', express.static(path.join(__dirname, '/public/css')))
+app.use('/js', express.static(path.join(__dirname, '/public/js')));
+app.use('/css', express.static(path.join(__dirname, '/public/css')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(8080, () => console.log('Running!'));
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,91 +1,90 @@
 function getPrices() {
-	return fetch('/api/prices', {
-		method: 'get'
-	}).then((res) => res.json()).then((data)=>{
-		console.log(data)
-		return data
-	}).catch(function(err) {
-		console.log(err);
-	});
+    return fetch('/api/prices', {
+        method: 'get'
+    }).then((res) => res.json()).then((data) => {
+        console.log(data);
+        return data
+    })
 }
 
 function getBalances() {
-	return fetch('/api/balances', {
-		method: 'get'
-	}).then((res) => res.json()).then((data)=>{
-		return data
-	}).catch(function(err) {
-		console.log(err);
-	});
+    return fetch('/api/balances', {
+        method: 'get'
+    }).then((res) => res.json()).then((data) => {
+        return data
+    })
 }
 
-let totals = [];
 let dates = [];
 let balances = {};
 let prices = {};
 getBalances().then((balancesData) => {
-	// initialize balances
-	let initialBalances = balancesData[0].balances;
-	for(let coin of Object.keys(initialBalances)){
-		balances[coin] = {};
-		balances[coin].balances = [];
-		balances[coin].hasBalance = false;
-		prices[coin] = [];
-	}
-	// extract balances
-	for(let b of balancesData){
-		for(let coin of Object.keys(b.balances)){
-			let balance = parseFloat(b.balances[coin].free) + parseFloat(b.balances[coin].locked);
-			if(balance > 0.0) balances[coin].hasBalance = true;
-			balances[coin].balances.push(balance);
-		}
-	}
-	// remove empty coins
-	for(let coin of Object.keys(balances)){
-		if(!balances[coin].hasBalance){
-			delete balances[coin];
-		}
-	}
-	return getPrices();
+    // initialize balances
+    let initialBalances = balancesData[0].balances;
+    for (let coin of Object.keys(initialBalances)) {
+        balances[coin] = {};
+        balances[coin].balances = [];
+        balances[coin].hasBalance = false;
+        prices[coin] = [];
+    }
+    // extract balances
+    for (let b of balancesData) {
+        for (let coin of Object.keys(b.balances)) {
+            let balance = parseFloat(b.balances[coin].free) + parseFloat(b.balances[coin].locked);
+            if (balance > 0.0) balances[coin].hasBalance = true;
+            balances[coin].balances.push(balance);
+        }
+    }
+    // remove empty coins
+    for (let coin of Object.keys(balances)) {
+        if (!balances[coin].hasBalance) {
+            delete balances[coin];
+        }
+    }
+    return getPrices();
 }).then((pricesData) => {
-	let i = 0;
-	for(let p of pricesData){
-		for(let coin of Object.keys(balances)){
-			if(coin === "BTC"){
-				pair = "BTCUSDT";
-				let price = balances[coin].balances[i] * parseFloat(p.prices.data[pair]);
-				prices[coin].push(price)
-			} else {
-				let pair = coin + "BTC";
-				let price = balances[coin].balances[i] * parseFloat(p.prices.data[pair] * p.prices.data["BTCUSDT"]);
-				prices[coin].push(price)
-			}
-		}
-		dates.push(p.prices.date);
-		i++;
-	}
+    // Calculate USDT value of balances according to prices
+    let i = 0;
+    for (let p of pricesData) {
+        for (let coin of Object.keys(balances)) {
+            if (coin === "BTC") {
+                let pair = "BTCUSDT";
+                let price = balances[coin].balances[i] * parseFloat(p.prices.data[pair]);
+                prices[coin].push(price)
+            } else {
+                let pair = coin + "BTC";
+                let price = balances[coin].balances[i] * parseFloat(p.prices.data[pair]) * parseFloat(p.prices.data["BTCUSDT"]);
+                prices[coin].push(price)
+            }
+        }
+        dates.push(p.prices.date);
+        i++;
+    }
 
-	for(let coin of Object.keys(balances)){
-		let h = document.createElement("h3");
-		h.innerHTML = coin;
-		document.body.appendChild(h);
-		let canvas = document.createElement("canvas");
-		canvas.id = "canvas-" + coin;
-		document.body.appendChild(canvas);
-		let ctx = canvas.getContext('2d');
-		let chart = new Chart(ctx, {
-			type: 'line',
-			data: {
-				labels: dates, 
-				datasets: [{
-					label: coin + " Value",
-					backgroundColor: 'rgb(255, 99, 132)',
-					borderColor: 'rgb(255, 99, 132)',
-					data: prices[coin],
-				}]
-			},
-			options: {}
-		});
-	}
-
+    // For every coin, draw a graph
+    for (let coin of Object.keys(balances)) {
+        let h = document.createElement("h3");
+        h.innerHTML = coin;
+        document.body.appendChild(h);
+        let canvas = document.createElement("canvas");
+        canvas.id = "canvas-" + coin;
+        document.body.appendChild(canvas);
+        let ctx = canvas.getContext('2d');
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: coin + " Value",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: prices[coin],
+                }]
+            },
+            options: {}
+        });
+    }
+}).catch((err)=> {
+    document.getElementById("errors").innerText = "Something went wrong, see console for error message.";
+    console.log(err);
 });
